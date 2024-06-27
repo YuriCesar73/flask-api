5bcf8546d0a037cf3e99d604112090aa97277574
Yuri Cesar <ycesar4273@gmail.com>
2024-06-27 19:39:27 -0300

    Adicionado try/except nas rotas.

--- app.py
+++ app.py
@@ -14,46 +14,83 @@ migrate = Migrate(app, db)
 
 @app.route('/tarefa', methods=['GET'])
 def get_tarefas():
-    tarefas = Tarefa.query.all()
-    return make_response(
-        jsonify([tarefa.as_dict() for tarefa in tarefas])
-    ) 
+    try:
+        tarefas = Tarefa.query.all()
+        return make_response(
+            jsonify([tarefa.as_dict() for tarefa in tarefas])
+        )
+    except:
+         return make_response(
+              jsonify(
+                   message='Ocorreu um erro'
+              ), 
+              400
+         ) 
 
 @app.route('/tarefa', methods=['POST'])
 def create_tarefa():
-    data = request.get_json()
-    novaTarefa = Tarefa(titulo=data['titulo'], descricao=data['descricao'], )
-    db.session.add(novaTarefa)
-    db.session.commit()
-    return make_response(
-        jsonify(
-            message='Tarefa criada com sucesso',
-            data=novaTarefa.as_dict()
-        ),
-        201
-    )
+    try:
+        data = request.get_json()
+        novaTarefa = Tarefa(titulo=data['titulo'], descricao=data['descricao'], )
+        db.session.add(novaTarefa)
+        db.session.commit()
+        return make_response(
+            jsonify(
+                message='Tarefa criada com sucesso',
+                data=novaTarefa.as_dict()
+            ),
+            201
+        )
+    except:
+         return make_response(jsonify(message='Ocorreu um erro'), 400)
 
 @app.route('/tarefa/<int:id>', methods=['GET'])
 def get_tarefa(id):
-    tarefa = Tarefa.query.get_or_404(id)
-    return jsonify(tarefa.as_dict())
+    try:
+        tarefa = Tarefa.query.get_or_404(id)
+        return jsonify(tarefa.as_dict())
+    except:
+         return make_response(
+              jsonify(
+                   message="Não existe tarefa com o id: {}" .format(id)
+              ),
+              404
+         )
 
 @app.route('/tarefa/<int:id>', methods=['PUT'])
 def update_tarefa(id):
-    tarefa = Tarefa.query.get_or_404(id)
-    data = request.get_json()
-    for key, value in data.items():
-            setattr(tarefa, key, value)
-    db.session.commit()
-    return jsonify({'message': 'Tarefa atualizada com sucesso!'})
+    try:
+        tarefa = Tarefa.query.get_or_404(id)
+        data = request.get_json()
+        for key, value in data.items():
+                setattr(tarefa, key, value)
+        db.session.commit()
+        return make_response(
+            jsonify(
+            {'message': 'Tarefa atualizada com sucesso!', 'data': tarefa.as_dict()}
+            ),
+            400 
+        ) 
+    except:
+        return make_response(
+            jsonify(message='Não existe tarefa com id: {}'.format(id)),
+            404
+        )
 
 @app.route('/tarefa/<int:id>', methods=['DELETE'])
 def delete_tarefa(id):
-    tarefa = Tarefa.query.get_or_404(id)
-    db.session.delete(tarefa)
-    db.session.commit()
-    return jsonify({'message': 'Tarefa removida com sucesso!'})
-
+    try:
+        tarefa = Tarefa.query.get_or_404(id)
+        db.session.delete(tarefa)
+        db.session.commit()
+        return jsonify({'message': 'Tarefa removida com sucesso!'})
+    except:
+      return make_response(
+          jsonify(
+              message="Não existe tarefa com id: {}".format(id)
+          ),
+          404
+      )   
 
 
 if __name__ == '__main__':
